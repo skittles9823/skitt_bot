37ef295081f30cd5a105b210aa248760043df66e
Rhyse Simpson <skat1@live.com.au>
2018-11-22 03:30:57 +1100

    reeee

--- skitt_bot/__init__.py
+++ skitt_bot/__init__.py
@@ -11,15 +11,10 @@ ENV = bool(os.environ.get('ENV', False))
 if ENV:
     TOKEN = os.environ.get('TOKEN', None)
     DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
-
 elif path.exists("skitt_bot/config.py"):
     from skitt_bot.config import Config
     TOKEN = Config.API_KEY
     DEEPFRY_TOKEN = Config.DEEPFRY_TOKEN
-
-else:
-    logger.error("config.py not found! Quitting.")
-    exit(-1)
 
 updater = telegram.ext.Updater(TOKEN)
 dispatcher = updater.dispatcher
