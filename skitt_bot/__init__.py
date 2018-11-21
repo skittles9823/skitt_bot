9162efd4d704cdce57ee2a11c641dc2e30d08ee2
Rhyse Simpson <skat1@live.com.au>
2018-11-22 03:14:25 +1100

    heroku haha yes

--- skitt_bot/__init__.py
+++ skitt_bot/__init__.py
@@ -5,10 +5,17 @@ import telegram.ext
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
-if path.exists("skitt_bot/config.py"):
+ENV = bool(os.environ.get('ENV', False))
+
+if ENV:
+    TOKEN = os.environ.get('TOKEN', None)
+    DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
+
+elif path.exists("skitt_bot/config.py"):
     from skitt_bot.config import Config
     TOKEN = Config.API_KEY
     DEEPFRY_TOKEN = Config.DEEPFRY_TOKEN
+    
 else:
     logger.error("config.py not found! Quitting.")
     exit(-1)
