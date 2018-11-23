b7a443d500b918f4f23dd479913aa50197f5941b
Rhyse Simpson <skat1@live.com.au>
2018-11-24 00:42:54 +1100

    format moar

--- skitt_bot/__main__.py
+++ skitt_bot/__main__.py
@@ -2,7 +2,7 @@ import telegram
 import importlib
 from skitt_bot.modules import ALL_MODULES
 from skitt_bot import dispatcher, logger, updater, TOKEN, WEBHOOK, LISTEN, CERT_PATH, PORT, URL, logger
-from telegram import Update, Bot
+from telegram import Update, Bot, ParseMode
 from telegram.ext import CommandHandler, run_async
 
 IMPORTED = {}
@@ -20,66 +20,55 @@ for module_name in ALL_MODULES:
 START_TEXT = """
 Hey fam! I'm {}, and I'm here to bring some funny maymays into your life!
 Check out /help for a full list of my commands with detailed descriptions.
+You can also check out the sourcecode for the bot [here](https://github.com/skittles9823/skitt_bot)
 """.format(dispatcher.bot.first_name)
 
 HELP_TEXT = """
 Ohai, I see you'd like to know what memes I have for sale.
 Well, here you go.
 
-Commands:
- <reply> = replying to a message
- <args> = adding a message after the command
- - /🅱:
-    - <reply> 
-        - replying to a message with replace a random character with the B emoji.
- - /👏:
-    - <reply> 
-        - adds clap emojis at the begining, end, and in every space in a message.
- - /😂:
-    - <reply> 
-        - a replica of mattatas copypasta command.
- - /deepfry:
-    - <reply> 
-        - for when your images/stickers need to get a little fried.
- - /dllm:
-    - <optional args> 
-        - sends a random chinese meme. if you add a number after the command, itll reply with a specific photo.
- - /forbes:
-    - <reply> 
-        - turns a message into a Forbes headline.
- - me too:
-    - Saying "me too" will have a small chance for the bot to make a remark.
- - /mock:
-    - <reply> 
-        - mocks a replied message lick the spongebob meme.
- - /owo:
-    - <reply> 
-        - OwO whats this? OwOfies a message.
- - /stretch:
-    - <reply> 
-        - stretches vowels in a message a random number of times.
- - /thonkify:
-    - <reply>/<args> 
-        - turns text into thonk text (only supports letters and none symbols for now).
- - /vapor:
-    - <reply>/<args> 
-        - turns a message into vaporwave text.
- - /zalgofy:
-    - <reply> 
-        - corrupts a message.
+*Commands:*
+ _<reply> = replying to a message_
+ _<args> = adding a message after the command_
+ - `/🅱`: - _<reply>_ 
+    *- replying to a message with replace a random character with the B emoji.*
+ - `/👏`: - _<reply>_ 
+    *- adds clap emojis at the begining, end, and in every space in a message.*
+ - `/😂`: - _<reply>_ 
+    *- a replica of mattatas copypasta command.*
+ - `/deepfry`: - _<reply>_ 
+    *- for when your images/stickers need to get a little fried.*
+ - `/dllm`: - _<optional args>_ 
+    *- sends a random chinese meme. if you add a number after the command, itll reply with a specific photo.*
+ - `/forbes`: - _<reply>_ 
+    *- turns a message into a Forbes headline.*
+ - `me too`:
+    *- Saying "me too" will have a small chance for the bot to make a remark.*
+ - `/mock`: - _<reply>_ 
+    *- mocks a replied message lick the spongebob meme.*
+ - `/owo`: - _<reply>_ 
+    *- OwO whats this? OwOfies a message.*
+ - `/stretch`: - _<reply>_ 
+    *- stretches vowels in a message a random number of times.*
+ - `/thonkify`: - _<reply>/<args>_ 
+    *- turns text into thonk text (only supports letters and none symbols for now).*
+ - `/vapor`: - _<reply>/<args>_ 
+    *- turns a message into vaporwave text.*
+ - `/zalgofy`: - _<reply>_ 
+    *- corrupts a message.*
 """
 
 @run_async
 def start(bot: Bot, update: Update):
     if update.effective_chat.type == "private":
-        update.effective_message.reply_text(START_TEXT)
+        update.effective_message.reply_text(START_TEXT, parse_mode=ParseMode.MARKDOWN)
     else:
         update.effective_message.reply_text("Waow sur, you've UwU-ken me :3")
 
 @run_async
 def help(bot: Bot, update: Update):
     if update.effective_chat.type == "private":
-        update.effective_message.reply_text(HELP_TEXT)
+        update.effective_message.reply_text(HELP_TEXT, parse_mode=ParseMode.MARKDOWN)
     else:
         update.effective_message.reply_text("Try this command again in a private message.")
 
